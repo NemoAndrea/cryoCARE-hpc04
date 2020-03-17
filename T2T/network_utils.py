8ec1777f16a945c21415170e6e114094faf388a4
Nemo Andrea <nemoandrea@gmail.com>
2020-03-17 12:58:40 +0100

    Improvements to 05 predict
    Now only require 1 file for both 2D and 3D, and automatic detection of dimensionality

---
+++ T2T/network_utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+from generate_train_data import normalize, compute_mean_std, denormalize
+
+
+def predict_tomogram(model, tomogram, mean, std, tiles = (8,8)):
+    print('> Applying denoiser to tomogram, using ' + str(model.config.n_dim) +'D network')  
+    print(model.config.axes)
+    print(model.config.axes[0:2])
+    if (model.config.n_dim == 3):
+         # axes for a slice network should be 'ZYX'
+        tomo_denoised = denormalize(model.predict(tomogram, axes=model.config.axes, n_tiles=(8,8,8), normalizer=None), mean, std)
+        
+        return tomo_denoised
+    elif (model.config.n_dim == 2):      
+        tomo_denoised = np.zeros(tomogram.shape) # intialise
+        num_tomo_slices = tomogram.shape[0] # how many z slices to process        
+        for i in range(num_tomo_slices):
+            print('Processing slice ' + str(i) + ' of ' + str(num_tomo_slices))
+            # axes for a slice network should be 'XY'
+            tomo_denoised[i,:,:] = denormalize(model.predict(tomogram[i,:,:], axes=model.config.axes[0:2], n_tiles=tiles, normalizer=None), mean, std)
+            
+        return tomo_denoised        
+    else:
+        raise Exception('Could not detect dimensionality of trained network')
